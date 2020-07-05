5cb1c0d7c60b83362522c4d61ed300afd97789b4
sherry <1936839308@qq.com>
2020-07-05 09:39:55 +0800

    deploy

---
+++ precache-manifest.a51faa4d59506008dc0a6656daf38288.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57e734f2e4288593295",
+    "url": "/money-website/css/app.96fee4e8.css"
+  },
+  {
+    "revision": "880ec3c6d63a95b38bec62e64443b996",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "b57e734f2e4288593295",
+    "url": "/money-website/js/app.c448151d.js"
+  },
+  {
+    "revision": "6043fea0e3c6d83c017a",
+    "url": "/money-website/js/chunk-vendors.5e3aae6b.js"
+  },
+  {
+    "revision": "d8834bdec6e60a8eecfa71856af49daa",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "61347b10267dab0ec4e4f83a2ac72437",
+    "url": "/money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
